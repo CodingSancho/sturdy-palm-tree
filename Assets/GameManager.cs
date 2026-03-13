19feaf00be5d92f69394fe700845922490163233
Sancho <lantosviktor22@gmail.com>
2026-03-13 04:33:11 +0100

    Adding autoincrement and upgrade button

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,12 +3,59 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public int score = 0;
+    public double score = 0;
+    public double incrementPerSecond = 1;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI incrementText;
+    public double incrementCost = 10;
+    public double incrementMultiplier = 1;
+    public TextMeshProUGUI upgradeButtonText;
 
-    public void AddScore()
+    void Start()
     {
-        score++;
-        scoreText.text = score.ToString();
+        InvokeRepeating("AutoIncrement", 1f, 1f);
+        UpdateScoreText();
+        UpdateIncrementText();
+    }
+
+    void AutoIncrement()
+    {
+        score += incrementPerSecond;
+        UpdateScoreText();
+    }
+
+    public void IncreaseIncrement()
+    {
+        if (score >= incrementCost)
+        {
+            score -= incrementCost;
+            incrementPerSecond += incrementMultiplier;
+            incrementCost *= 1.5;
+            UpdateScoreText();
+            UpdateIncrementText();
+        }
+    }
+
+    void UpdateScoreText()
+    {
+        scoreText.text = FormatNumber(score);
+    }
+
+    void UpdateIncrementText()
+    {
+        incrementText.text = "Rate: " + FormatNumber(incrementPerSecond);
+        upgradeButtonText.text = "Upgrade (" + FormatNumber(incrementCost) + ")";
+    }
+
+    string FormatNumber(double number)
+    {
+        if (number >= 1_000_000_000)
+            return (number / 1_000_000_000D).ToString("0.##") + "B";
+        else if (number >= 1_000_000)
+            return (number / 1_000_000D).ToString("0.##") + "M";
+        else if (number >= 1_000)
+            return (number / 1_000D).ToString("0.##") + "k";
+        else
+            return number.ToString("0");
     }
 }
